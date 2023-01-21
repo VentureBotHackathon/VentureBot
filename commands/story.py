6242f749aac8b996ce86ac7fc8ddf0a91600863f
Callum <callumbrezden@gmail.com>
2023-01-21 14:15:07 -0800

    Added Story Dropdown View

---
+++ commands/story.py
@@ -0,0 +1,46 @@
+import nextcord
+from nextcord import Interaction
+from nextcord.ext import commands
+
+class Story(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @nextcord.slash_command(description="Begin a new story and start voting")
+    async def story(self, interaction: Interaction):
+        await interaction.send("Message", view=StoryDropdown(timeout=None))
+
+class StoryDropdown(nextcord.ui.View):
+    @nextcord.ui.select(
+        placeholder='Please choose a story',
+
+        min_values=1,
+        max_values=1,
+
+        options =[
+
+            nextcord.SelectOption(
+                label = "NWHacks 2023",
+                emoji = "🔌",
+                description = "Explore the hackathon and become a winner!",
+                value='nwhacks'
+            ),
+
+            nextcord.SelectOption(
+                label = "Story #2",
+                emoji = "⚙️",
+                description = "2nd Story to be added",
+                value='story2'
+            )
+        ]
+    )
+
+    async def callback(self, select, interaction: nextcord.Interaction):
+        if (select.values[0] == "nwhacks"):
+            await interaction.send("You choosed NWHacks!")
+
+        elif (select.values[0] == "story2"):
+            await interaction.send("You choosed story2")
+
+def setup(bot):
+    bot.add_cog(Story(bot))
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import os
-import sys
 import json
-import datetime
-import pymongo
 import nextcord
 from nextcord.ext import commands
 
@@ -15,9 +12,11 @@ with open("./config.json") as x:
 
 @bot.event
 async def on_ready():
-    # Prints to console telling us that it is logged in.
     print(f'Logged in as {bot.user}\n')
-    # Status for the bot.
-    await bot.change_presence(status=nextcord.Status.online, activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="/story"))
+    await bot.change_presence(status=nextcord.Status.dnd, activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="/story"))
+
+for filename in os.listdir('./commands'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'commands.{filename[:-3]}')
 
 bot.run(configData["TOKEN"])
